1a5b51fd1b06522ffd482160d9f605e16dbcdee7
Jayreg3 <jeregistre@gmail.com>
2019-03-20 22:22:10 -0300

    Removed all extra unnecessary materials and code.

--- server.js
+++ server.js
@@ -12,32 +12,13 @@ const port = 3000;
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
-const API_KEY = "3b8367f0-b77e-11e8-bf0e-e9322ccde4db";
-const API_NASA = "DEMO_KEY";
-
-let comments = {};
 
 // behavior for the index route
-app.g
-
 app.get("/", (req, res) => {
-    const url = `https://api.harvardartmuseums.org/gallery?size=100&apikey=${API_KEY}`;
-    const url2 = `https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY`;
-    /*fetch(url)
-        .then(response => response.json())
-        .then(data => {
-            console.error("ok");
-            res.render("index", { galleries: data.records });
-        });*/
-    fetch(url2)
-        .then(response => response.json())
-        .then(data => {
-            res.render("index", { galleries2: data, test: "testing" });
-            console.log(JSON.stringify(data) + "already did");
-
-        });
+    res.render("index");
 });
 
+//behavior for the search route, get method used to allow users to bookmark and return to a search
 app.get("/search",function(req,res){
     var search = {
         general_search : req.query.general_search,
@@ -46,59 +27,14 @@ app.get("/search",function(req,res){
         location : req.query.location,
         nasa_center : req.query.nasa_center
     }
-    console.log(search);
     const url = `https://images-api.nasa.gov/search?q=${search.general_search}&title=${search.title}&description=${search.description}&location=${search.location}&center=${search.nasa_center}`;
-    //fetch('https://images-api.nasa.gov/search?q=rock&description=Tune%20into%20Third%20Rock%20Radio%20for%20The%20Joe%20Show%20starring%20Joe%20Acaba%20as%20Guest%20DJ%20on%20Thursday,%20December%207th%20at%205pm')
     fetch(url)
         .then(response => response.json())
         .then(data => {
-           res.render("search",{userValue : search, test: "testing", nasa :data});
-           console.log(JSON.stringify(data.collection.items[0].data[0].description) + "just did");
-           data.collection.items.forEach(object => {
-            console.log(JSON.stringify(object.data[0].title.length) + "loop");
-           });
-       });
-    //res.json(student);
-});
-
-app.get("/gallery/:gallery_id", function(req, res) {
-    const url = `https://api.harvardartmuseums.org/object?gallery=${
-        req.params.gallery_id
-    }&apikey=${API_KEY}`;
-    fetch(url)
-        .then(response => response.json())
-        .then(data => {
-            res.render("gallery", { objects: data.records });
+           res.render("search",{userValue : search, nasa :data});
         });
 });
 
-app.get("/object/:object_id", function(req, res) {
-    const url = `https://api.harvardartmuseums.org/object/${
-        req.params.object_id
-    }?apikey=${API_KEY}`;
-    fetch(url)
-        .then(response => response.json())
-        .then(data => {
-            res.render("object", {
-                object: {
-                    data,
-                    comments: comments[req.params.object_id]
-                        ? comments[req.params.object_id]
-                        : ["There are no comments!"]
-                }
-            });
-        });
-});
-
-app.post("/object/:object_id", function(req, res) {
-    let commentArr = comments[req.params.object_id];
-    if (commentArr) {
-        commentArr.push(req.body.comment);
-    } else {
-        comments[req.params.object_id] = [req.body.comment];
-    }
-    res.redirect(req.originalUrl);
-});
 
 app.listen(port, host, () => {
     console.log(`Server running on http://${host}:${port}/`);
